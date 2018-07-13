08090f813823ffa4f4275cc4e61fb72e2bf8cc5f
André Tupinambá <andrelrt@gmail.com>
2018-07-13 14:45:26 +0100

    Fixed signed,unsigned comparision

--- test/simdtest/simd_compare_test.cpp
+++ test/simdtest/simd_compare_test.cpp
@@ -18,33 +18,33 @@ TEST(SimdCompareTest, LessThanSSE)
     // cmp       = {40, 30, 20, 10}
     // cmp[i], i =   3   2   1   0
 
-    EXPECT_EQ( 0x0000, sa::greater_than_mask< int32_t >(  5, cmp ) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x0000u, sa::greater_than_mask< int32_t >(  5, cmp ) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t >( 5, cmp );
-    EXPECT_EQ( 0x000f, sa::greater_than_mask< int32_t >( 15, cmp ) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x000fu, sa::greater_than_mask< int32_t >( 15, cmp ) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t >( 15, cmp );
-    EXPECT_EQ( 0x00ff, sa::greater_than_mask< int32_t >( 25, cmp ) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x00ffu, sa::greater_than_mask< int32_t >( 25, cmp ) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t >( 25, cmp );
-    EXPECT_EQ( 0x0fff, sa::greater_than_mask< int32_t >( 35, cmp ) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x0fffu, sa::greater_than_mask< int32_t >( 35, cmp ) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t >( 35, cmp );
-    EXPECT_EQ( 0xffff, sa::greater_than_mask< int32_t >( 45, cmp ) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0xffffu, sa::greater_than_mask< int32_t >( 45, cmp ) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t >( 45, cmp );
 
-    EXPECT_EQ( 0, sa::greater_than_index< int32_t >(  5, cmp ) );
-    EXPECT_EQ( 0, sa::greater_than_index< int32_t >( 10, cmp ) );
-    EXPECT_EQ( 1, sa::greater_than_index< int32_t >( 15, cmp ) );
-    EXPECT_EQ( 1, sa::greater_than_index< int32_t >( 20, cmp ) );
-    EXPECT_EQ( 2, sa::greater_than_index< int32_t >( 25, cmp ) );
-    EXPECT_EQ( 2, sa::greater_than_index< int32_t >( 30, cmp ) );
-    EXPECT_EQ( 3, sa::greater_than_index< int32_t >( 35, cmp ) );
-    EXPECT_EQ( 3, sa::greater_than_index< int32_t >( 40, cmp ) );
-    EXPECT_EQ( 4, sa::greater_than_index< int32_t >( 45, cmp ) );
+    EXPECT_EQ( 0u, sa::greater_than_index< int32_t >(  5, cmp ) );
+    EXPECT_EQ( 0u, sa::greater_than_index< int32_t >( 10, cmp ) );
+    EXPECT_EQ( 1u, sa::greater_than_index< int32_t >( 15, cmp ) );
+    EXPECT_EQ( 1u, sa::greater_than_index< int32_t >( 20, cmp ) );
+    EXPECT_EQ( 2u, sa::greater_than_index< int32_t >( 25, cmp ) );
+    EXPECT_EQ( 2u, sa::greater_than_index< int32_t >( 30, cmp ) );
+    EXPECT_EQ( 3u, sa::greater_than_index< int32_t >( 35, cmp ) );
+    EXPECT_EQ( 3u, sa::greater_than_index< int32_t >( 40, cmp ) );
+    EXPECT_EQ( 4u, sa::greater_than_index< int32_t >( 45, cmp ) );
 
     pCmp[3] = 0x00007891;
     pCmp[2] = 0x00005576;
     pCmp[1] = 0x000041a7;
     pCmp[0] = 0x00002b83;
 
-    EXPECT_EQ( 0x000f, sa::greater_than_mask< int32_t >( 0x41a7, cmp ) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x000fu, sa::greater_than_mask< int32_t >( 0x41a7, cmp ) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t >( 0x41a7, cmp );
 }
 
@@ -65,41 +65,41 @@ TEST(SimdCompareTest, LessThanAVX)
     // cmp       = {80, 70, 60, 50, 40, 30, 20, 10}
     // cmp[i], i =   7   6   5   4   3   2   1   0
 
-    EXPECT_EQ( 0x00000000, (sa::greater_than_mask< int32_t, sa::avx_tag >(  5, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x00000000u, (sa::greater_than_mask< int32_t, sa::avx_tag >(  5, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >(  5, cmp );
-    EXPECT_EQ( 0x0000000f, (sa::greater_than_mask< int32_t, sa::avx_tag >( 15, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x0000000fu, (sa::greater_than_mask< int32_t, sa::avx_tag >( 15, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >( 15, cmp );
-    EXPECT_EQ( 0x000000ff, (sa::greater_than_mask< int32_t, sa::avx_tag >( 25, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x000000ffu, (sa::greater_than_mask< int32_t, sa::avx_tag >( 25, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >( 25, cmp );
-    EXPECT_EQ( 0x00000fff, (sa::greater_than_mask< int32_t, sa::avx_tag >( 35, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x00000fffu, (sa::greater_than_mask< int32_t, sa::avx_tag >( 35, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >( 35, cmp );
-    EXPECT_EQ( 0x0000ffff, (sa::greater_than_mask< int32_t, sa::avx_tag >( 45, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x0000ffffu, (sa::greater_than_mask< int32_t, sa::avx_tag >( 45, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >( 45, cmp );
-    EXPECT_EQ( 0x000fffff, (sa::greater_than_mask< int32_t, sa::avx_tag >( 55, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x000fffffu, (sa::greater_than_mask< int32_t, sa::avx_tag >( 55, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >( 55, cmp );
-    EXPECT_EQ( 0x00ffffff, (sa::greater_than_mask< int32_t, sa::avx_tag >( 65, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x00ffffffu, (sa::greater_than_mask< int32_t, sa::avx_tag >( 65, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >( 65, cmp );
-    EXPECT_EQ( 0x0fffffff, (sa::greater_than_mask< int32_t, sa::avx_tag >( 75, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0x0fffffffu, (sa::greater_than_mask< int32_t, sa::avx_tag >( 75, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >( 75, cmp );
-    EXPECT_EQ( 0xffffffff, (sa::greater_than_mask< int32_t, sa::avx_tag >( 85, cmp )) ) << "hex: 0x" << std::hex
+    EXPECT_EQ( 0xffffffffu, (sa::greater_than_mask< int32_t, sa::avx_tag >( 85, cmp )) ) << "hex: 0x" << std::hex
         << std::setw(8) << std::setfill( '0' ) << sa::greater_than_mask< int32_t, sa::avx_tag >( 85, cmp );
 
-    EXPECT_EQ( 0, (sa::greater_than_index< int32_t, sa::avx_tag >(  5, cmp )) );
-    EXPECT_EQ( 0, (sa::greater_than_index< int32_t, sa::avx_tag >( 10, cmp )) );
-    EXPECT_EQ( 1, (sa::greater_than_index< int32_t, sa::avx_tag >( 15, cmp )) );
-    EXPECT_EQ( 1, (sa::greater_than_index< int32_t, sa::avx_tag >( 20, cmp )) );
-    EXPECT_EQ( 2, (sa::greater_than_index< int32_t, sa::avx_tag >( 25, cmp )) );
-    EXPECT_EQ( 2, (sa::greater_than_index< int32_t, sa::avx_tag >( 30, cmp )) );
-    EXPECT_EQ( 3, (sa::greater_than_index< int32_t, sa::avx_tag >( 35, cmp )) );
-    EXPECT_EQ( 3, (sa::greater_than_index< int32_t, sa::avx_tag >( 40, cmp )) );
-    EXPECT_EQ( 4, (sa::greater_than_index< int32_t, sa::avx_tag >( 45, cmp )) );
-    EXPECT_EQ( 4, (sa::greater_than_index< int32_t, sa::avx_tag >( 50, cmp )) );
-    EXPECT_EQ( 5, (sa::greater_than_index< int32_t, sa::avx_tag >( 55, cmp )) );
-    EXPECT_EQ( 5, (sa::greater_than_index< int32_t, sa::avx_tag >( 60, cmp )) );
-    EXPECT_EQ( 6, (sa::greater_than_index< int32_t, sa::avx_tag >( 65, cmp )) );
-    EXPECT_EQ( 6, (sa::greater_than_index< int32_t, sa::avx_tag >( 70, cmp )) );
-    EXPECT_EQ( 7, (sa::greater_than_index< int32_t, sa::avx_tag >( 75, cmp )) );
-    EXPECT_EQ( 7, (sa::greater_than_index< int32_t, sa::avx_tag >( 80, cmp )) );
-    EXPECT_EQ( 8, (sa::greater_than_index< int32_t, sa::avx_tag >( 85, cmp )) );
+    EXPECT_EQ( 0u, (sa::greater_than_index< int32_t, sa::avx_tag >(  5, cmp )) );
+    EXPECT_EQ( 0u, (sa::greater_than_index< int32_t, sa::avx_tag >( 10, cmp )) );
+    EXPECT_EQ( 1u, (sa::greater_than_index< int32_t, sa::avx_tag >( 15, cmp )) );
+    EXPECT_EQ( 1u, (sa::greater_than_index< int32_t, sa::avx_tag >( 20, cmp )) );
+    EXPECT_EQ( 2u, (sa::greater_than_index< int32_t, sa::avx_tag >( 25, cmp )) );
+    EXPECT_EQ( 2u, (sa::greater_than_index< int32_t, sa::avx_tag >( 30, cmp )) );
+    EXPECT_EQ( 3u, (sa::greater_than_index< int32_t, sa::avx_tag >( 35, cmp )) );
+    EXPECT_EQ( 3u, (sa::greater_than_index< int32_t, sa::avx_tag >( 40, cmp )) );
+    EXPECT_EQ( 4u, (sa::greater_than_index< int32_t, sa::avx_tag >( 45, cmp )) );
+    EXPECT_EQ( 4u, (sa::greater_than_index< int32_t, sa::avx_tag >( 50, cmp )) );
+    EXPECT_EQ( 5u, (sa::greater_than_index< int32_t, sa::avx_tag >( 55, cmp )) );
+    EXPECT_EQ( 5u, (sa::greater_than_index< int32_t, sa::avx_tag >( 60, cmp )) );
+    EXPECT_EQ( 6u, (sa::greater_than_index< int32_t, sa::avx_tag >( 65, cmp )) );
+    EXPECT_EQ( 6u, (sa::greater_than_index< int32_t, sa::avx_tag >( 70, cmp )) );
+    EXPECT_EQ( 7u, (sa::greater_than_index< int32_t, sa::avx_tag >( 75, cmp )) );
+    EXPECT_EQ( 7u, (sa::greater_than_index< int32_t, sa::avx_tag >( 80, cmp )) );
+    EXPECT_EQ( 8u, (sa::greater_than_index< int32_t, sa::avx_tag >( 85, cmp )) );
 }
 
